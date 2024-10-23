b6706a89f31b265135ca4c1670d5a9daaefcf87a
spacewulf <kwolterstorff531@gmail.com>
2024-10-23 19:39:18 -0400

    Updated with some more wezterm config

--- wezterm.lua
+++ wezterm.lua
@@ -1,9 +1,27 @@
 local wezterm = require("wezterm")
+
 local config = wezterm.config_builder()
-require("colors")
 
-config.color_scheme_dirs = { "/home/$USER/.config/wezterm/colors" }
+local colorscheme = require("colors.carbon")
 
-config.color_scheme = "carbon"
+config.font = wezterm.font("CaskaydiaCove Nerd Font Mono")
+config.font_size = 12
 
-config.font = "CaskaydiaCove Nerd Font"
+config.window_padding = {
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0,
+}
+
+config.default_prog = { "tmux" }
+
+config.enable_tab_bar = false
+
+-- config.window_decorations = "RESIZE"
+
+config.colors = colorscheme
+
+config.window_background_opacity = 0.8
+
+return config
