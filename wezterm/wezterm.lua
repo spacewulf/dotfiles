b903fcf28aff52ceb6787fb8ede5ee893c2da1b3
spacewulf <kwolterstorff531@gmail.com>
2024-10-25 16:28:47 -0400

    Added neovim configuration to the repo.

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,34 +1,3 @@
---[[
-local wezterm = require("wezterm")
-
-local config = wezterm.config_builder()
-
-local colorscheme = require("colors.carbon")
-
-config.font = wezterm.font("CaskaydiaCove Nerd Font Mono")
-config.font_size = 12
-
-config.window_padding = {
-	left = 0,
-	right = 0,
-	top = 0,
-	bottom = 0,
-}
-
-config.default_prog = { "tmux" }
-
-config.enable_tab_bar = false
-
--- config.window_decorations = "RESIZE"
-
-config.colors = colorscheme
-
-config.window_background_opacity = 0.8
-
-return config
-]]
---
-
 local Config = require("config")
 
 require("events.right-status").setup()
